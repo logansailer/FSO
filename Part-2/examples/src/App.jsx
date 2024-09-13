88c3c653f4dba0c909ad4eb480eba21ab51394c3
Logan <logansailer0633@gamil.com>
2024-09-13 01:06:19 -0400

    added footer

--- Part-2/examples/src/App.jsx
+++ Part-2/examples/src/App.jsx
@@ -1,23 +1,8 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 import noteService from "./services/notes";
-
-const Footer = () => {
-  const footerStyle = {
-    color: "green",
-    fontStyle: "italic",
-    fontSize: 16,
-  };
-  return (
-    <div style={footerStyle}>
-      <br />
-      <em>
-        Note app, Department of Computer Science, University of Helsinki 2024
-      </em>
-    </div>
-  );
-};
 
 const App = () => {
   const [notes, setNotes] = useState([]);
